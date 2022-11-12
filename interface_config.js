a603b50cfcf5c16de5ed36c95bfc3ec40d8eb4f8
Genius-Corner <67021686+Genius-Corner@users.noreply.github.com>
2022-11-12 16:22:49 +0530

    Changed icon and label

--- interface_config.js
+++ interface_config.js
@@ -100,7 +100,7 @@ var interfaceConfig = {
     MAXIMUM_ZOOMING_COEFFICIENT: 1.3,
 
     /**
-     * Whether the mobile app Jitsi Meet is to be promoted to participants
+     * Whether the mobile app My Mobiforce is to be promoted to participants
      * attempting to join a conference in a mobile Web browser. If
      * {@code undefined}, defaults to {@code true}.
      *
